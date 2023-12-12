d0187156a5f0af94dbf0d159e6955200fc30cf41
Ryan <ryankane180@gmail.com>
2023-12-12 21:33:40 +0000

    Buggy

--- Java/Sorting/src/QuickSort.java
+++ Java/Sorting/src/QuickSort.java
@@ -1,2 +1,56 @@
 public class QuickSort {
+
+    public static void main(String[] args) {
+        int[] myArray = new int[]{78,33,22,55,44,99,04,67,100,2};
+        qs(myArray, 0, myArray.length-1);
+        for(int i : myArray){
+            System.out.println(i);
+        }
+    }
+
+    public static void qs(int[] array, int low, int high){
+
+        if(low >= high){
+            return;
+        }
+
+        int pivot = array[array.length - 1];
+        int leftPointer = partition(array, low, high, pivot);
+
+        qs(array, low, leftPointer-1);
+        qs(array, leftPointer+1, high);
+
+    }
+
+    public static int partition(int[] array, int low, int high, int pivot){
+
+        int leftPointer = low;
+        int rightPointer = high-1;
+
+        while(leftPointer < rightPointer){
+            while(array[leftPointer] <= pivot && leftPointer < rightPointer){
+                leftPointer++;
+            }
+            while(array[rightPointer] >= pivot && leftPointer < rightPointer){
+                rightPointer--;
+            }
+
+            int temp = array[leftPointer];
+            array[leftPointer] = array[rightPointer];
+            array[rightPointer] = temp;
+
+        }
+
+        if(array[leftPointer] > array[high]) {
+            int temp = array[leftPointer];
+            array[leftPointer] = array[high];
+            array[high] = temp;
+        } else {
+            leftPointer = high;
+        }
+
+        return leftPointer;
+
+    }
+
 }
