21b16ec55614b940e1b412b33b015e68f5907fcb
Ryan <ryankane180@gmail.com>
2023-12-13 14:33:05 +0000

    Makes more sense in my head this way. No more partition method

--- Java/Sorting/src/QuickSort.java
+++ Java/Sorting/src/QuickSort.java
@@ -1,38 +1,21 @@
 public class QuickSort {
 
     public static void main(String[] args) {
-        int[] myArray = new int[]{78,33,22,55,44,99,4,67,100,2};
-        qs(myArray, 0, myArray.length-1);
+        int[] myArray = new int[]{78,33,22,55,44,99,4,67,100,2,3};
+        qsort(myArray, 0, myArray.length-1);
         for(int i : myArray){
             System.out.println(i);
         }
     }
 
-    public static void qs(int[] array, int low, int high){
-
+    public static void qsort(int[] array, int low, int high){
         if(low >= high){
             return;
         }
 
-        int pivotIndex = array.length - 1;
-        int pivot = array[pivotIndex];
-        int temp = array[pivotIndex];
-        array[pivotIndex] = array[high];
-        array[high] = temp;
-        int leftPointer = partition(array, low, high, pivot);
-
-
-
-        qs(array, low, leftPointer-1);
-        qs(array, leftPointer+1, high);
-
-
-    }
-
-    public static int partition(int[] array, int low, int high, int pivot){
-
+        int pivot = array[high];
         int leftPointer = low;
-        int rightPointer = high-1;
+        int rightPointer = high;
 
         while(leftPointer < rightPointer){
             while(array[leftPointer] <= pivot && leftPointer < rightPointer){
@@ -45,19 +28,16 @@ public class QuickSort {
             int temp = array[leftPointer];
             array[leftPointer] = array[rightPointer];
             array[rightPointer] = temp;
-
         }
 
-        if(array[leftPointer] > array[high]) {
-            int temp = array[leftPointer];
-            array[leftPointer] = array[high];
-            array[high] = temp;
-        } else {
-            leftPointer = high;
-        }
+        int temp = array[leftPointer];
+        array[leftPointer] = array[high];
+        array[high] = temp;
 
-        return leftPointer;
+        qsort(array, low, leftPointer-1);
+        qsort(array, leftPointer+1, high);
 
     }
+
 
 }
