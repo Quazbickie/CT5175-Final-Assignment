4fbc66849892dee544c6d4f6b508b1d6c45a8b96
Ryan <ryankane180@gmail.com>
2024-01-03 16:03:29 +0000

    Logic works for most part. Edge cases to be fixed and printing

--- Java/Sorting/src/NumberSequence.java
+++ Java/Sorting/src/NumberSequence.java
@@ -1,9 +1,37 @@
 public class NumberSequence {
 
-
     public static void main(String[] args) {
-        
-    }
 
+        int numRows = 25;
+
+        int pascalsTriangle[][] = new int[numRows][numRows];
+
+        pascalsTriangle[0][0] = 1;
+        pascalsTriangle[1][0] = 1;
+        pascalsTriangle[1][1] = 1;
+
+        for(int i = 2; i < numRows; i++){
+
+            int[] currentRow = pascalsTriangle[i];
+
+            for(int j = 0; j < currentRow.length; j++) {
+                if(j == 0 || j == currentRow.length){
+                    currentRow[j] = 1;
+                }
+                else{
+                    currentRow[j] = pascalsTriangle[i-1][j-1] + pascalsTriangle[i-1][j];
+                }
+            }
+            pascalsTriangle[i] = currentRow;
+        }
+
+        for(int i = 0; i < pascalsTriangle.length; i++){
+            for(int j = 0; j < pascalsTriangle[i].length; j++){
+                System.out.print(pascalsTriangle[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+
+    }
 
 }
