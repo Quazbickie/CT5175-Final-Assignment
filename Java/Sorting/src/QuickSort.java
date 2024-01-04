742b43a7d37c4ecc89b6b4634680a17aed3bf01c
Ryan <ryankane180@gmail.com>
2024-01-04 16:01:54 +0000

    Comments
    Comments for code submission

--- Java/Sorting/src/QuickSort.java
+++ Java/Sorting/src/QuickSort.java
@@ -8,15 +8,35 @@ public class QuickSort {
         }
     }
 
+
     public static void qsort(int[] array, int low, int high){
+
+        //'Base Case'
+        //if the low element is equal or greater
+        //than the high element then it is time to
+        //return
         if(low >= high){
             return;
         }
 
+        //set pivot element as the 'high' index in the array
+        //The pivot element is important in quicksort. After each partition, or
+        //iteration, the pivot element will be in its correct and final position
+        //in the array.
         int pivot = array[high];
+
+        //set left and right pointers to low and high element respectively.
         int leftPointer = low;
         int rightPointer = high;
 
+        //We have a left and right pointer, each at opposite ends of the array
+        //we are going to increment the leftpointer until we find an element that is
+        //greater than the pivot element. Once we find this element, or we meet the rightpointer,
+        //we stop.
+        //Then we are going to decrement the rightpointer until we find an element that is
+        //less than the pivot element. Once we find this element, or we meet the leftpointer,
+        //we stop.
+        //We then swap the elements the left and right pointer are pointing to.
         while(leftPointer < rightPointer){
             while(array[leftPointer] <= pivot && leftPointer < rightPointer){
                 leftPointer++;
@@ -27,7 +47,14 @@ public class QuickSort {
             swap(array, leftPointer, rightPointer);
         }
 
+        //then we put the pivot element in its correct and final position in the array
         swap(array, leftPointer, high);
+
+        //recursively call this method on each side of the array,
+        //from index 0 to the leftpointer
+        //and from the leftpointer+1 to the last index
+        //Essentially, the large array is segmented into smaller arrays,
+        //each of which will be sorted using the above methods.
         qsort(array, low, leftPointer-1);
         qsort(array, leftPointer+1, high);
 
--- Java/Sorting/src/SelectionSort.java
+++ Java/Sorting/src/SelectionSort.java
@@ -7,6 +7,10 @@ public class SelectionSort {
     public static void SelectionSort(){
         int[] myArray = new int[]{12,3,2,4,5,1,123,66,43,54,78,89,96,43,22,11};
 
+        //Starting at the first element in the array,
+        //we step through the array and locate the index
+        //of the lowest element. We then put this in its correct
+        //position in the array.
         for(int i = 0; i < myArray.length; i++){
             int lowestIdx = i;
             for(int j = i+1; j < myArray.length; j++){
